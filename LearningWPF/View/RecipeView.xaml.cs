cbfe2806ab62d83fd4b6477b6a90c18dc449727e
212 <212@DESKTOP-7S3H9O6>
2022-03-06 16:58:12 +0300

    Fix

--- LearningWPF/View/RecipeView.xaml.cs
+++ LearningWPF/View/RecipeView.xaml.cs
@@ -9,10 +9,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LearningWPF.View
 {
@@ -48,7 +44,6 @@ namespace LearningWPF.View
         {
 
         }
-
         private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
         {
                         if(e.Key== Key.F1) 
